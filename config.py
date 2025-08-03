525172bfcf077ce0b0983e85ee101dac388cc162
rjshvjy <rjshvjy@gmail.com>
2025-08-03 11:08:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-DB_URL = "postgresql://postgres:Ufc%401570@db.jkhyjizbfxhmhopqweat.supabase.co:6543/postgres?pgbouncer=true"
-
+DB_URL = "postgresql://postgres:Ufc%401570@db.jkhyjizbfxhmhopqweat.supabase.co:5432/postgres?sslmode=require"
 # Replace [YOUR_PASSWORD] and [YOUR_PROJECT_REF] with your Supabase details from Settings > Database
+
 
 

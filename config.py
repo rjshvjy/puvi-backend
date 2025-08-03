48a565a094044c67a93d1be0512f0d4e57bcee69
rjshvjy <rjshvjy@gmail.com>
2025-08-03 11:39:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-postgresql://postgres:Ufc%401570@db.jkhyjizbfxhmhopqweat.supabase.co:5432/postgres
+DB_URL = "postgresql://postgres.jkhyjizbfxhmhopqweat:Ufc%401570@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
 # Replace [YOUR_PASSWORD] and [YOUR_PROJECT_REF] with your Supabase details from Settings > Database
+
 
 
 

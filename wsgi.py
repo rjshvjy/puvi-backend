6af7be78e2273e31f4e80527ccf43d089269d017
rjshvjy <rjshvjy@gmail.com>
2025-08-04 08:16:31 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from purchase_endpoints import app
+"""
+WSGI entry point for PUVI Oil Manufacturing System
+Used by Gunicorn for production deployment
+"""
+
+from app import app
 
 if __name__ == "__main__":
     app.run()
